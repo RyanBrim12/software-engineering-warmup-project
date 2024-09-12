1ac978c8e3727e9b8997b911fe2b165e3f315511
awrigh <abigail.wright@uvm.edu>
2024-09-12 17:29:18 -0400

    Fixed parser to handle int type and called firebase class

--- query.py
+++ query.py
@@ -1,7 +1,4 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from firebase_admin import firestore_async
+import firebase
 
 def tokenize(sentence):
     # Split the sentence into rough tokens
@@ -9,6 +6,7 @@ def tokenize(sentence):
     result = []
     inside_quote = False
     current_quote = []
+
 
     # Loop through all tokens
     for token in tokens:
@@ -40,14 +38,14 @@ def tokenize(sentence):
     
     return result
 
-# Find the symbol used in the query to correctly call get from firebase
-# TODO: Handle 2 queries at Once? Should this return Boolean  true, if the array is found. False if the array is empty?
-# Main should handle this with the 'and' and 'or' catches
+# Find the symbol used in the query to correctly call get from firebase, return collection
 
-def find_query(query):
+def find_query(query, connect):
     # IF query is length 3, example: "title, "==", "tenet", contiue else print try again
     if (len(query) == 3):
         
+        queryResult = []
+
         # keywords for fields
         keywords = ["director", "title", "genre", "duration", "rating", "writer", "release date"]
         
@@ -55,253 +53,55 @@ def find_query(query):
         if (query[0] in keywords):
 
             # comparison opperators list 
-            comparisonOpperators = [">", "<", ">=", "<=", "==", "of", "not"]
+            comparisonOpperators = [">", "<", ">=", "<=", "==", "of"]
 
             # if the 2nd token is in the list continue and send to corresponding functions else print try again
             if (query[1] in comparisonOpperators):
+                
+                # if query operator is of 
+                if( query[1] == "of"):
 
-                if(query[1] == "<"):
-                    less_than(query)
+                    queryResult = connect.complete_query(query[0], "==", query[2])
 
-                elif(query[1] == "<="):
-                    less_than_equal_to(query)
+                # check if keyword isn't an integer variable
+                elif ( query[0] != "duration" and query[0] != "rating"):
 
-                elif(query[1] == ">"):
-                    greater_than(query)
+                    # check if comparison is not < > >= <=
+                    if (query[1] != "==" or query[1] != "of"):
 
-                elif(query[1] == ">="):
-                    greater_than_equal_to(query)
+                        print(f"Cannot use \"{query[1]}\" with keyword: {query[0]}")
+                        
+                    else:
 
-                elif(query[1] == "=="):
-                    equal_to(query)
-
-                elif(query[1] == "not"):
-                    not_in(query)
-
-                elif(query[1] == "of"):
-                    of(query)
+                        queryResult = connect.complete_query(query[0], query[1], query[2])
+                
+                #  else complete query
                 else:
-                    print(f"{query[1]}: not a comparrison opperator in the system\nTry again or type Help for examples\n\n")
-
+                    queryResult = connect.complete_query(query[0], query[1], query[2])
+                
             else:
-                print(f"{query[0]}: not a field in the system\nTry again or type Help for examples\n\n")
+                print(f"{query[1]}: not a comparrison opperator in the system\nTry again or type Help for examples\n\n")
+
+        else:
+            print(f"{query[0]}: not a field in the system\nTry again or type Help for examples\n\n")
 
     else:
 	
         print ("Query input of wrong size! Try again or type Help for examples\n\n")
     
-    return 0
-
-# TODO: Should these return booleans true if array found, false if array is empty?
-# These could also have count totals printed at the begining of the search: example: Total Movies Found: 28
-# Should these return the queries? To work with line 360? Or should we change line 360 and have another function to deal with 2 operators?
-
-# This function compares fields less than to the desription and prints out console. Uses Firebase Connection!
-def less_than(q):
-     # Initializing query fields needed: field and description
-    field = q[0]
-    
-    description = q[-1]
-    
-    # Initializing the Collection From Database
-    collection_group = db.collection("movies")
-    
-    # Where Clause what to find from database
-    query = collection_group.where(field,"<", description).order_by(field, "DESCENDING");
-    
-    # Call the database
-    results = query.get()
-
-    # foreach query within the collection, turn to dictionary print formatted nicely 
-    for result in results:
-        single_querys= result.to_dict()
-
-        print("\n\n"+single_querys["title"])
-
-        single_querys.pop("title")
-
-        for key, value in single_querys.items():
-        
-            print(f"{key}: {value}\n")
-
-    
-# This function compares fields less than equal to the desription and prints out console. Uses Firebase Connection!
-def less_than_equal_to(q):
-     # Initializing query fields needed: field and description
-    field = q[0]
-    
-    description = q[-1]
-    
-    # Initializing the Collection From Database
-    collection_group = db.collection("movies")
-    
-    # Where Clause what to find from database
-    query = collection_group.where(field,"<=", number).order_by(field, "DESCENDING");
-    
-    # Call the database
-    results = query.get()
-
-    # foreach query within the collection, turn to dictionary print formatted nicely 
-    for result in results:
-        single_querys= result.to_dict()
-
-        print("\n\n"+single_querys["title"])
-
-        single_querys.pop("title")
-
-        for key, value in single_querys.items():
-        
-            print(f"{key}: {value}\n")
-
-    
-# This function compares fields greater than to the desription and prints out console. Uses Firebase Connection!
-def greater_than(q):
-     # Initializing query fields needed: field and description
-    field = q[0]
-    
-    description = q[-1]
-    
-    # Initializing the Collection From Database
-    collection_group = db.collection("movies")
-    
-    # Where Clause what to find from database
-    query = collection_group.where(field,">", number).order_by(field, "DESCENDING");
-    
-    # Call the database
-    results = query.get()
-
-    # foreach query within the collection, turn to dictionary print formatted nicely 
-    for result in results:
-        single_querys= result.to_dict()
-
-        print("\n\n"+single_querys["title"])
-
-        single_querys.pop("title")
-
-        for key, value in single_querys.items():
-        
-            print(f"{key}: {value}\n")
-
-    
-# This function compares fields greater than equal to the desription and prints out console. Uses Firebase Connection!
-def greater_than_equal_to(q):
-     # Initializing query fields needed: field and description
-    field = q[0]
-    
-    description = q[-1]
-    
-    # Initializing the Collection From Database
-    collection_group = db.collection("movies")
-    
-    # Where Clause what to find from database
-    query = collection_group.where(field,">=", number).order_by(field, "DESCENDING");
-    
-    # Call the database
-    results = query.get()
-
-    # foreach query within the collection, turn to dictionary print formatted nicely 
-    for result in results:
-        single_querys= result.to_dict()
-
-        print("\n\n"+single_querys["title"])
-
-        single_querys.pop("title")
-
-        for key, value in single_querys.items():
-        
-            print(f"{key}: {value}\n")
-
-
-# This function compares fields equal to the desription and prints out console. Uses Firebase Connection!
-def equal_to(q):
-    
-    # Initializing query fields needed: field and description
-    field = q[0]
-    
-    description = q[-1]
-    
-    # Initializing the Collection From Database
-    collection_group = db.collection("movies")
-
-    # Where Clause, what to find from database
-    querys = collection_group.where(field,"==", description).order_by(field, "DESCENDING");
-
-    # Call the database with clause
-    query_snapshot = querys.get()
-
-    # foreach query within the collection, turn to dictionary print formatted nicely 
-    for query in query_snapshot:
-
-        single_query= query.to_dict()
-
-        print("\n\n"+single_query["title"])
-
-        single_query.pop("title")
-
-        for key, value in single_query.items():
-        
-            print(f"{key}: {value}\n")
-
-
-# TODO: Implement Not_in: I wasn't sure what was meant by this --Abi
-def not_in(q):
-    feild = q[0]
-	
-    description = q[-1]
-	
-    collection_group = db.collection("movies")
-	
-    querys = collection_group(feild, "!=", description).order_by(feild, "DESCENDING")
-	
-    query_snapshot = querys.get()
-	
-    for query in query_snapshot:
-
-        single_query= query.to_dict()
-
-        print("\n\n"+single_query["title"])
-
-        single_query.pop("title")
-
-        for key, value in single_query.items():
-        
-            print(f"{key}: {value}\n")
-		
-
-# This function finds fields equal to the desription and prints out console. Uses Firebase Connection!
-# Uses title like > Director of Tenet, has title hardcoded. Will not work for anyother given field. But prints the field it wants.
-def of(q):
-    
-     # Initializing query fields needed: field and description
-    field = q[0]
-    
-    description = q[-1]
-    
-    # Initializing the Collection From Database
-    collection_group = db.collection("movies")
-    
-    # Where Clause what to find from database
-    query = collection_group.where("title","==", description).order_by(field, "DESCENDING");
-
-    # Call the database
-    results = query.get()
-
-    # foreach query within the collection, turn to dictionary print formatted nicely 
-    for result in results:
-
-        single_querys= result.to_dict()
-
-        print("\n\n"+single_querys[field])
+    return queryResult
 
 
 if __name__ == "__main__":
+
+    # create instance of Firebase_Connection
+    firebaseConnect = firebase.Firebase_Connection("movies")
     
     while True:
         # Get user input
         sentence = input("> ").lower()
-
+    
         # Check input
-        # TODO: Does the NOT make sense or do we want "!="? Do we want " " around numbers? example: rating > "3"?
         if sentence == "help":
             # This is the Help Menu, with examples of what the expected input should be 
             print("Help Menu:\n\n")
@@ -316,7 +116,7 @@ if __name__ == "__main__":
             print("Key Words: \n")
             print("OF \nThis key word will find a specific attribute of a movie\nExample: Director OF \"Ratatouille\"\n\n")
             print("AND \nThis key word will find multiple specified attributes of a movie with \nExample: Director AND Duration OF \"Ratatouille\"\n\n")
-            print("NOT \nThis key word will find all but the specified attributes of a movie left out  \nExample:  Duration > \"90\" NOT > Director = \"Christopher Nolan\"\n\n")
+            
         elif sentence == "quit":
             break
         elif sentence.count('\"') % 2 != 0:
@@ -340,11 +140,21 @@ if __name__ == "__main__":
             list_after_and = tokenized[and_index+1:]
 
             # Call 2 seperate functions for each side of and
-            result1 = find_query(list_before_and)
-            result2 = find_query(list_after_and)
+            result1 = find_query(list_before_and, firebaseConnect)
+            result2 = find_query(list_after_and, firebaseConnect)
 
             # Find intersection
             result = [item for item in result1 if item in result2]
+
+            if(len(result) == 0):
+                print ("Query Not Found, Try Again!")
+            
+            else:
+
+                # Print Results
+                for r in result:
+
+                    print(firebaseConnect)
 
         elif "or" in tokenized:
             # Find the index of 'or'
@@ -355,12 +165,35 @@ if __name__ == "__main__":
             list_after_or = tokenized[and_index+1:]
 
             # Call 2 seperate functions for each side of 'of'
-            result1 = find_query(list_before_or)
-            result2 = find_query(list_after_or)
+            result1 = find_query(list_before_or, firebaseConnect)
+            result2 = find_query(list_after_or, firebaseConnect)
 
             # Find union
             result = list(result1.symmetric_difference(result2))
 
+            if(len(result) == 0):
+                print ("Query Not Found, Try Again!")
+            
+            else:
+
+                # Print Results
+                for r in result:
+
+                    print(firebaseConnect)
+
         else:
             # Call function for corresponding query
-            result = find_query(tokenized)
+            result = find_query(tokenized, firebaseConnect)
+
+            if(len(result) == 0):
+                print ("Query Not Found, Try Again!")
+            
+            else:
+
+                # Print Results
+                for r in result:
+
+                    print(firebaseConnect)
+
+            
+            
