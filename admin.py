156393325633d9ac58aaed6596cd05b512363b07
RyanBrim12 <12rab@optonline.net>
2024-09-12 15:15:03 -0400

    Sets id as string not int

--- admin.py
+++ admin.py
@@ -7,7 +7,7 @@ def get_movies_from_file(file_name):
         file_contents = json.load(f)
     movies = []
     for movie in file_contents:
-        movies.append(Movie(movie["ID"], movie["Title"],
+        movies.append(Movie(str(movie["ID"]), movie["Title"],
                             movie["Release Date"], movie["Rating"],
                             movie["Directed by"], movie["Written by"],
                             movie["Duration"], movie["Genres"]))
@@ -15,7 +15,7 @@ def get_movies_from_file(file_name):
 
 
 if __name__ == "__main__":
-    fb = Firebase_Connection()
+    fb = Firebase_Connection("movies")
     movies = get_movies_from_file("movies.json")
     fb.delete_collection()
     fb.create_collection(movies)
